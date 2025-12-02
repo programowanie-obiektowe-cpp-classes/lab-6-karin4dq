d634f7d30f8effff07b4e4bb7e0852d33d390867
Karina Dudko <133782806+karin4dq@users.noreply.github.com>
2025-12-02 21:27:53 +0100

    Update foo.hpp

--- foo.hpp
+++ foo.hpp
@@ -6,11 +6,23 @@
 #include <list>
 #include <vector>
 
-std::vector< char > foo(std::list< Human >& people)
+std::vector<char> foo(std::list<Human>& people)
 {
-    std::vector< char > retval(people.size());
+    std::vector<char> retval(people.size());
+    auto itOut = retval.begin(); // iterator po elem. wyjściowych
 
-    // Twoja implementacja tutaj
+    std::for_each(people.begin(), people.end(),
+        [&](Human& h)
+        {
+            h.birthday();  // analogicznie jak w oryginale
+
+            *itOut++ = (h.isMonster() ? 'n' : 'y');
+
+
+        }
+    );
+
+    std::reverse(retval.begin(), retval.end());
 
     return retval;
 }
